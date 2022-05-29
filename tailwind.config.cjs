90152fbbdb5d9d8d1d18797af510a77e14e11282
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 21:47:25 -0700

    remove max width from tailwind type

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,19 @@
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+  
 	theme: {
-		extend: {}
+	  extend: {
+		typography: {
+		  DEFAULT: {
+			css: {
+			  maxWidth: null,
+			},
+		  },
+		},
+	  },
 	},
-
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('daisyui'),
-	]
-};
-
-module.exports = config;
+  
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  }
+  
+  module.exports = config
