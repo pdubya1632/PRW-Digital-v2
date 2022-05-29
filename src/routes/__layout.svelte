917b15db0919bba7666491e5347a57fb2e0f9339
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 22:36:27 -0700

    strip default styles in prep for using tailwindcss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,20 +6,9 @@
 
 <!-- src/routes/__layout.svelte -->
 
-<!-- <nav>
-	<div class="nav-container">
-		<a href="/" class="nav-logo" title="Back to Home">Patrick Richardson Wright</a>
-		<div class="nav-links">
-			<a href="/about" class="link">About</a>
-			<a href="/projects" class="link">Projects</a>
-			<a href="/contact" class="link">Contact</a>
-		</div>
-	</div>
-</nav> -->
-
 <Navbar />
 
-<main class="container max-w-3xl mx-auto px-4 mb-20">
+<main class="container mx-auto px-4 mb-20">
 	<slot />
 </main>
 
