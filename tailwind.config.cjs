d9724361cbec6f279976f2ef01d2a2989e0d5fcc
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 22:50:09 -0700

    add daisyui themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,23 @@
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-  
-	theme: {
-	  extend: {
-		typography: {
-		  DEFAULT: {
-			css: {
-			  maxWidth: null,
-			},
-		  },
-		},
-	  },
-	},
-  
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('daisyui')
-	],
-  }
-  
-  module.exports = config
 
-// module.exports = {
-// 	daisyui: {
-// 	  themes: ["light", "dark", "cmyk"],
-// 	},
-//   }
+	theme: {
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						maxWidth: null
+					}
+				}
+			}
+		}
+	},
+
+	daisyui: {
+		themes: ['light', 'dark', 'cmyk']
+	},
+
+	plugins: [require('@tailwindcss/typography'), require('daisyui')]
+};
+
+module.exports = config;
