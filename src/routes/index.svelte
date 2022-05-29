b4418033db4d8e4140e11783782e2697067a6205
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 23:04:38 -0700

    temp hide img

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,11 @@
 
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col lg:flex-row-reverse">
-	  <!-- <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
-	  <div> -->
+	  <!-- <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" /> -->
+	  <div>
 		<h1 class="text-5xl font-bold">Creative Coder</h1>
 		<p class="py-6">Blurb here</p>
 		<button class="btn btn-primary">Contact</button>
 	  </div>
 	</div>
-  </div>
+</div>
