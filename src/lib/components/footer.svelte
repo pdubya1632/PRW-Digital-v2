e63a635460015af1bca583001c9422a9ef8ab2b7
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 22:21:57 -0700

    add footer component and twitter svg

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,21 @@
+<script>
+	import TwitterIcon from './twitter-icon.svelte';
+</script>
+
+<footer class="footer p-10 bg-neutral text-neutral-content">
+	<div>
+		<!-- insert logo here -->
+		<p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+		<p>
+			Copyright &copy; {`${new Date().getFullYear()}`} - All right reserved by Patrick Richardson Wright
+		</p>
+	</div>
+	<div>
+		<span class="footer-title">Social</span>
+		<div class="grid grid-flow-col gap-4">
+			<a href="https://twitter.com">
+				<TwitterIcon />
+			</a>
+		</div>
+	</div>
+</footer>
