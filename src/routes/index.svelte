c4385ae6d3fdf9eea53c7de04fc935406c2d8e6f
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 23:03:40 -0700

    add daisyui hero code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,13 @@
 	<title>The Portfolio of Patrick Richardson Wright</title>
 </svelte:head>
 
-<h1>Code + Design</h1>
-
-<h2 class="subtitle">I'm a creative developer with an entrepreneurial streak</h2>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+	  <!-- <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
+	  <div> -->
+		<h1 class="text-5xl font-bold">Creative Coder</h1>
+		<p class="py-6">Blurb here</p>
+		<button class="btn btn-primary">Contact</button>
+	  </div>
+	</div>
+  </div>
