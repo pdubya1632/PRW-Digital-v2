9acf0dec47d5174e40969e9c60a6da799e1e223b
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-28 22:30:31 -0700

    create / import navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,26 @@
 <script>
+	import Navbar from '$lib/components/navbar.svelte'
 	import Footer from '$lib/components/footer.svelte'
 	import '../app.css'
   </script>
 
 <!-- src/routes/__layout.svelte -->
-<nav>
+
+<!-- <nav>
 	<div class="nav-container">
 		<a href="/" class="nav-logo" title="Back to Home">Patrick Richardson Wright</a>
 		<div class="nav-links">
 			<a href="/about" class="link">About</a>
-			<!-- <a href="/blog" class="link">Blog</a> -->
 			<a href="/projects" class="link">Projects</a>
 			<a href="/contact" class="link">Contact</a>
 		</div>
 	</div>
-</nav>
+</nav> -->
+
+<Navbar />
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
-	<!-- Pages will be injected below -->
 	<slot />
 </main>
+
+<Footer />
