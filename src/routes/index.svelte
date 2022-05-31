804e9d97172d565b45d938df8ca155da1d7dcd6e
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-30 23:14:00 -0700

    constrain width of hero text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 	<div class="hero-content flex-col lg:flex-row-reverse">
 	  <!-- <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" /> -->
 	  <div>
-		<h1 class="text-8xl font-bold">Creative Developer</h1>
+		<h1 class="text-8xl font-bold max-w-xs">Creative Developer</h1>
 		<!-- <p class="py-6">Blurb here</p>
 		<button class="btn btn-primary">Contact</button> -->
 	  </div>
