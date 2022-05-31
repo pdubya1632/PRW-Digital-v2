05d4158d49f27006d31ab94fb81a3aec8002b5bf
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-30 23:06:30 -0700

    update font, color theme, and nav / footer content

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,52 +1,21 @@
-<div class="navbar bg-base-100 sticky top-0 z-10">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<label tabindex="0" class="btn btn-ghost lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
-			</label>
-			<ul
-				tabindex="0"
-				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-			>
-				<li><a href="about">About</a></li>
-				<!-- <li tabindex="0">
-					<a class="justify-between"> Parent </a>
-					<ul class="p-2">
-						<li><a>Submenu 1</a></li>
-						<li><a>Submenu 2</a></li>
-					</ul>
-				</li> -->
-				<li><a href="projects">Projects</a></li>
-			</ul>
-		</div>
-		<a class="btn btn-ghost normal-case text-xl" href="/">Patrick Richardson Wright</a>
+<div class="navbar ">
+	<div class="flex-1">
+	  <a class="btn btn-ghost normal-case text-xl" href="/">Patrick Richardson Wright</a>
 	</div>
-	<div class="navbar-center hidden lg:flex">
-		<ul class="menu menu-horizontal p-0">
-			<li><a href="projects">Projects</a></li>
-			<!-- <li tabindex="0">
-				<a> Parent </a>
-				<ul class="p-2">
-					<li><a>Submenu 1</a></li>
-					<li><a>Submenu 2</a></li>
-				</ul>
-			</li> -->
-			<li><a href="about">About</a></li>
-		</ul>
+	<div class="flex-none">
+	  <ul class="menu menu-horizontal p-0">
+		<li><a href="/work">Work</a></li>
+		<!-- <li tabindex="0">
+		  <a>
+			Parent
+			<svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+		  </a>
+		  <ul class="p-2 bg-base-100">
+			<li><a>Submenu 1</a></li>
+			<li><a>Submenu 2</a></li>
+		  </ul>
+		</li> -->
+		<li><a href="/about">About</a></li>
+	  </ul>
 	</div>
-	<div class="navbar-end">
-		<a class="btn">Contact</a>
-	</div>
-</div>
+  </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,13 @@
 	<title>The Portfolio of Patrick Richardson Wright</title>
 </svelte:head>
 
-<div class="hero min-h-screen bg-base-200">
+<div class="hero min-h-screen">
 	<div class="hero-content flex-col lg:flex-row-reverse">
 	  <!-- <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" /> -->
 	  <div>
-		<h1 class="text-5xl font-bold">Creative Coder</h1>
-		<p class="py-6">Blurb here</p>
-		<button class="btn btn-primary">Contact</button>
+		<h1 class="text-8xl font-bold">Creative Developer</h1>
+		<!-- <p class="py-6">Blurb here</p>
+		<button class="btn btn-primary">Contact</button> -->
 	  </div>
 	</div>
 </div>
