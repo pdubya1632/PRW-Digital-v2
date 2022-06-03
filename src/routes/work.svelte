53442ec54ee7292d1d16da4f3d5195360f75e525
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-03 06:26:12 -0700

    update project links

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -5,13 +5,13 @@
 
 <section class="container mx-auto">
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
+		<div class="flex justify-center text-2xl border-2 border-gray-300 p-4 bg-gray-100">
 			<a href="https://pdubya1632.github.io/Get-Outside/">Get Outside</a>
 		</div>
-		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
-			<a href="https://pdubya1632.github.io/UWCB-06-Homework/index.html">Weather Dashboard</a>
+		<div class="flex justify-center text-2xl border-2 border-gray-300 p-4 bg-gray-100">
+			<a href="https://pdubya1632.github.io/Weather-Dashboard/">Weather Dashboard</a>
 		</div>
-		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
+		<div class="flex justify-center text-2xl border-2 border-gray-300 p-4 bg-gray-100">
 			<a href="https://pdubya1632.github.io/Javascript-Coding-Quiz/">Coding Quiz</a>
 		</div>
 	</div>
