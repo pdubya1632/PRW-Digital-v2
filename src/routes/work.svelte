08bdcdfbf396fd6c7109390d49a26b680d94ebd0
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-03 06:08:56 -0700

    add projects to work page

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -6,16 +6,13 @@
 <section class="container mx-auto">
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
 		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
-			<a href="#">Project 1</a>
+			<a href="https://pdubya1632.github.io/Get-Outside/">Get Outside</a>
 		</div>
 		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
-			<a href="#">Project 2</a>
+			<a href="https://pdubya1632.github.io/UWCB-06-Homework/index.html">Weather Dashboard</a>
 		</div>
 		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
-			<a href="#">Project 3</a>
-		</div>
-		<div class="flex justify-center text-4xl border-2 border-gray-300 p-6 bg-gray-100">
-			<a href="#">Project 4</a>
+			<a href="https://pdubya1632.github.io/Javascript-Coding-Quiz/">Coding Quiz</a>
 		</div>
 	</div>
 </section>
